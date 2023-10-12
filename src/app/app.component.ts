d0a26d7c6ccad3df88ae3bf0aede7b65e74b78b3
fatih efkan <mfefkan@hotmail.com>
2023-10-12 11:51:02 +0300

    NgContent (<ng-content>)  ile componentler arası veri aktarımı örneklerle incelendi...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { CommonModule, NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [HomeComponent,CommonModule]
 })
 export class AppComponent {
   title = 'NgContent';
